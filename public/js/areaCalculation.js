5d663c144b11afe2be4fa1e45b2cc5044bb95047
DeKillerz7 <lisda_love12@outlook.com>
2022-06-22 05:01:32 +0700

    Check price and area calculation(not finished)

--- public/js/areaCalculation.js
+++ public/js/areaCalculation.js
@@ -1,48 +1,20 @@
-import axios from "axios";
 import pointInPolygon from 'point-in-polygon'
 
+const areas = {
+    bangkok: {
+        bangkokA: [[99.189629, 16.0498747], [99.9312213, 14.6590091], [100.3241505, 13.9175539], [102.2257817, 13.6837549], [103.187032, 14.6799661], [102.7931295, 16.0909143], [100.2165147, 16.7822506], [99.3429304, 16.5109029], [99.189629, 16.0498747]],
+        bangkokB: [[-83.19, 25.774], [-68.118, 18.466], [-66.757, 32.321]]
+    }
+}
+
 //geocoding API
-export const areaCalculation = async (address) => {
+export const areaCalculation = (coordinates, areaType) => {
+    const area = Object.keys(areas[areaType]).find(area => pointInPolygon(coordinates, areas[areaType][area]))
+    console.log(area)
     //polygon coordinates [x, y]
     const triangleCoords = [[-80.19, 25.774], [-66.118, 18.466], [-64.757, 32.321]];
     //point coordinates [x, y]
     const point = [-70.31294531249999, 26.43079719103903]
-    console.log(pointInPolygon(point, triangleCoords))
 
-    //getting google maps api library for polygon
-    // try {
-    //     const loader = new Loader({
-    //         apiKey: "",
-    //         libraries: ["geometry"]
-    //       });
-
-    //     loader.load().then((googlemaps) => {
-    //         //polygon coorinates
-    //         const triangleCoords = [
-    //             { lat: 25.774, lng: -80.19 },
-    //             { lat: 18.466, lng: -66.118 },
-    //             { lat: 32.321, lng: -64.757 },
-    //         ];
-            
-    //         //converting latLng to geometrical coordinates
-    //         const currPosition = new googlemaps.maps.LatLng( 25.774, -80.19 )
-    //         console.log(currPosition)
-
-    //         //creating polygon
-    //         const bermudaTriangle = new googlemaps.maps.Polygon({ paths: triangleCoords });
-            
-    //         //check if address's in polygon
-    //         const result = googlemaps.maps.geometry.poly.containsLocation(
-    //             currPosition,
-    //             bermudaTriangle
-    //         )
-
-    //         return result
-    //     }).catch((err) => {
-    //         console.log(err)
-    //     });
-
-    // } catch (err) {
-    //     return err
-    // }
+    return area
 }
--- routes/webhook.js
+++ routes/webhook.js
@@ -2,31 +2,86 @@ import express from 'express'
 import { areaCalculation } from '../public/js/areaCalculation.js'
 import { geocodingAPI } from '../public/js/geocodingAPI.js'
 import { sheetsInfo, readSheets, writeSheets } from '../public/js/sheetsRequest.js'
+import NodeCache  from "node-cache"
+import areaPrices from '../areaPrices.json'
 
-//init router
+//init packages
 const router = express.Router()
+const myCache = new NodeCache();
 
 router.get('/', async (req, res) => {
-  res.send(areaCalculation())
+  res.send(areaCalculation([100.21329, 16.04008], "bangkok"))
 })
 
 //webhook from dialogflow
 router.post('/', async (req, res) => {
-  console.log(req.body.originalDetectIntentRequest.payload)
-  // const address = await geocodingAPI(req.body.queryResult.queryText)
-  // const obj = {
-  //   fulfillmentMessages: [
-  //     req.body.queryResult.fulfillmentMessages[0],
-  //     {
-  //       text: {
-  //         text: [
-  //           address.data.results[0].formatted_address
-  //         ]
-  //       }
-  //     }
-  //   ]
-  // }
-  // res.send(obj)
+  //checking and storing events that have been done
+  const events = []
+  const reqResult = req.body.queryResult
+
+  //Checking if the user is booking or checking price
+  switch (req.body.queryResult.action.split(".")[0]) {
+    case "Booking":
+      
+      break;
+  
+    case "PriceCheck":
+      const result = await priceCheck(reqResult, events)
+      console.log(typeof result)
+      if (typeof result === "object" || typeof result === "string") {
+        const obj = {
+          fulfillmentMessages: [
+            {
+              text: {
+                text: [
+                  typeof result === "object" ? 
+                  `Sedan car: ${result.sedan} baht\nFamily car: ${result.family} baht\nMinibus: ${result.minibus} baht`
+                  :
+                  result
+                ]
+              }
+            }
+          ]
+        }
+        res.send(obj)
+      }
+      break;
+
+    default:
+
+      myCache.set("key1", events)
+      break;
+  }
 })
 
 export default router
+
+const priceCheck = async (reqResult, events) => {
+  const eventCount = events.length
+  let areaType = ""
+
+  //convert to real address
+  const address = await geocodingAPI(reqResult.queryText)
+  //finding province
+  const results = address.data.results[0]
+  console.log(results)
+  areaType = results.address_components.find(item => item.types.includes("administrative_area_level_1")).long_name.toLowerCase()
+  
+  //checking which area the address is in
+  const areaResult = areaCalculation([results.geometry.location.lat, results.geometry.location.lng], areaType)
+  const objTemp = [
+    ...events,
+    {
+      address: results,
+      area: {
+        type: areaType,
+        result: areaResult
+      }
+    }
+  ]
+  myCache.set("key1", objTemp)
+  if (eventCount === 2) {
+    return areaPrices[areaType][objTemp[0].area.result][objTemp[1].area.result] || "We don't have service in that area yet."
+  }
+  return false
+}
