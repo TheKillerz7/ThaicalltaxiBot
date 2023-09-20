5efc0f77e73a21fffdf9812f9049af6ddb842d31
David Shu <mstudym13@gmail.com>
2023-09-20 15:33:09 +0700

    change success message

--- lineComponents/afterConfirm.js
+++ lineComponents/afterConfirm.js
@@ -1,64 +1,70 @@
 exports.afterConfirm = (bookingId, roomId) => {
-    const card = {
-        "type": "bubble",
-        "size": "kilo",
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": `Succeeded!`,
-              "weight": "bold",
-              "color": "#1DB446",
-              "size": "lg"
-            },
-            {
-              "type": "text",
-              "text": `Your Booking Code:`,
-              "margin": "md",
-              "wrap": true
-            },
-            {
-              "type": "text",
-              "text": `${bookingId}`,
-              "weight": "bold",
-              "color": "#1DB446",
-              "wrap": true
-            }
-          ],
-          "paddingBottom": "xs"
+  const card = {
+    type: "bubble",
+    size: "kilo",
+    body: {
+      type: "box",
+      layout: "vertical",
+      contents: [
+        {
+          type: "text",
+          text: `Perfect`,
+          weight: "bold",
+          color: "#1DB446",
+          size: "lg",
         },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
+        {
+          type: "text",
+          text: `Driver will chat with you ASAP!`,
+          margin: "md",
+          wrap: true,
+        },
+        {
+          type: "text",
+          text: `Your Booking Code:`,
+          margin: "md",
+          wrap: true,
+        },
+        {
+          type: "text",
+          text: `${bookingId}`,
+          weight: "bold",
+          color: "#1DB446",
+          wrap: true,
+        },
+      ],
+      paddingBottom: "xs",
+    },
+    footer: {
+      type: "box",
+      layout: "vertical",
+      spacing: "sm",
+      contents: [
+        {
+          type: "box",
+          layout: "vertical",
+          contents: [
             {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "button",
-                  "style": "link",
-                  "action": {
-                    "type": "uri",
-                    "label": "Chat Now",
-                    "uri": `https://liff.line.me/1657246657-zkQEeOoL?roomId=${roomId}`
-                  },
-                  "color": "#ffffff",
-                  "height": "sm"
-                }
-              ],
-              "margin": "md",
-              "backgroundColor": "#1e3a8a",
-              "cornerRadius": "md"
-            }
+              type: "button",
+              style: "link",
+              action: {
+                type: "uri",
+                label: "Chat Now",
+                uri: `https://liff.line.me/1657246657-zkQEeOoL?roomId=${roomId}`,
+              },
+              color: "#ffffff",
+              height: "sm",
+            },
           ],
-          "flex": 0,
-          "paddingTop": "none"
-        }
-      }
+          margin: "md",
+          backgroundColor: "#1e3a8a",
+          cornerRadius: "md",
+        },
+      ],
+      flex: 0,
+      paddingTop: "none",
+    },
+  };
 
-    return card
-}
+  return card;
+};
