47697b8786182d9bc325b3afe06807b458585634
David Shu <mstudym13@gmail.com>
2024-01-16 19:59:47 +0700

    database changed

--- models/knexdb.js
+++ models/knexdb.js
@@ -1,12 +1,23 @@
-const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-      host : 'eu-cdbr-west-03.cleardb.net',
-      port : 3306,
-      user : 'b9bdaa155b903e',
-      password : '8ce298fd',
-      database : 'heroku_3c6b299d53dcdbe'
-    }
-  });
+// const knex = require('knex')({
+//     client: 'mysql',
+//     connection: {
+//       host : 'eu-cdbr-west-03.cleardb.net',
+//       port : 3306,
+//       user : 'b9bdaa155b903e',
+//       password : '8ce298fd',
+//       database : 'heroku_3c6b299d53dcdbe'
+//     }
+//   });
 
-module.exports = knex
+const knex = require("knex")({
+  client: "mysql",
+  connection: {
+    host: "eu-cluster-west-01.k8s.cleardb.net",
+    port: 3306,
+    user: "b685b6f6c48afd",
+    password: "aea17206",
+    database: "heroku_bf88614f130d16e",
+  },
+});
+
+module.exports = knex;
